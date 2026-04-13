78a814addad937ce11e955cfbdfd845671934158
Alia <singhalia2008@gmail.com>
2026-04-13 19:03:28 +0530

    Add files via upload

---
+++ FCC #1.py
@@ -0,0 +1,78 @@
+
+print("FCC stuff")
+
+#Lists
+Fruits = ["Apple", "Banana", "Cherry"]
+print(Fruits[0]) #Apple
+print(Fruits[1]) #Banana
+
+
+#Tuples
+Coordinates = (10.0, 20.0)
+print(Coordinates[0]) #10.0
+print(Coordinates[1]) #20.0
+
+
+#Dictionaries
+Student = {"Name": "Aluu", 
+           "Age":92, 
+           "Marks": "97%"
+}
+
+print(Student["Age"])
+
+
+#Sets
+#NO DUPLICATES club
+A= {8,4,6,0}
+B= {6,9,0,3} #INTERSECTION
+print(A & B) # {0, 6}
+
+
+#Integers
+my_integer_var= 11
+print("Integer:", my_integer_var) #11
+
+
+#Floats
+my_float_var= 1.11 #integer with decimal point
+print("Float:", my_float_var) #1.11
+
+#Booleans(true or false)
+my_boolean_var= True
+print("Boolean:", my_boolean_var) #True
+
+#example of boolean
+username = "aluu"
+password = "1234"
+
+if username == "aluu" and password == "1234":
+    print("Login successful")
+else:
+    print("Wrong credentials")
+
+#Range
+my_range_var = range(11) 
+print("Range:", my_range_var) #range(0, 11) 
+
+#example of range
+for i in range(11):
+    print(i)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    
