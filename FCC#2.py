2cd90387bbcedb7b1c3def0a3e29020261512676
Alia <singhalia2008@gmail.com>
2026-04-15 00:21:30 +0530

    Add files via upload

---
+++ FCC#2.py
@@ -0,0 +1,13 @@
+print("REPROT CARD PRINTER")
+
+name = "Alice"
+print(name, type(name))
+
+is_student = True
+print(is_student, type(is_student))
+
+age = 20
+print(age, type(age))
+
+score = 80.5
+print(isinstance(score, float))
