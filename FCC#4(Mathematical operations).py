84bd0314570a51a50e8a6a5383d00eed789f9d1d
Alia <singhalia2008@gmail.com>
2026-05-08 13:28:40 +0530

    Add integer addition example to mathematical operations
    Added integer addition operation and output.

--- FCC#4(Mathematical operations).py
+++ FCC#4(Mathematical operations).py
@@ -8,3 +8,7 @@ my_int_2= -11
 
 print(type(my_int_1))# <class 'int'>
 print(type(my_int_2)) # <class 'int'>
+
+#Addition with integers
+sum_ints= my_int_1 + my_int_2
+print('Integer Addition: ', sum_ints)
