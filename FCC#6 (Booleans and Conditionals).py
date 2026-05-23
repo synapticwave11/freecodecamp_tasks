a84f2b154d5fc8f47997094c9fa39155b615e015
Alia <singhalia2008@gmail.com>
2026-05-24 01:20:54 +0530

    Add files via upload

---
+++ FCC#6 (Booleans and Conditionals).py
@@ -0,0 +1,21 @@
+print('Conditional Statements and Logical Operators')
+#Conditional statements
+print(3 > 4) # False
+print(3 < 4) # True
+print(3 == 4) # False
+print(4 == 4) # True
+print(3 != 4) # True
+print(3 >= 4) # False
+print(3 <= 4) # True
+
+#If or Else statements
+
+
+
+
+age = 12
+
+if age >= 18:
+    print('You are an adult')
+else:
+    print('You are not an adult yet') # You are not an adult yet
